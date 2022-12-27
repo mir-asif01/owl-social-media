bf16d411d6a3d6af734c1942958f9fa7f8bdf01e
asif3062 <asifbsb2002@gmail.com>
2022-12-27 18:03:43 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWhUyMD6hG_do1XfHLGRi23o_W1OM0ItA",
+  authDomain: "owl-social-media.firebaseapp.com",
+  projectId: "owl-social-media",
+  storageBucket: "owl-social-media.appspot.com",
+  messagingSenderId: "734754596836",
+  appId: "1:734754596836:web:6aa764ee8da1ee011709f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
